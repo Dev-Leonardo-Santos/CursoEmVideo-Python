43c8f5ef4855bd20fdbbbbe04b517628322f728c
Leonardo Santos <leo.o.d3v@gmail.com>
2026-03-05 18:41:39 -0300

    fix: corrige contagem regressiva do EXE046

--- Exercicios/Mundo 2/EXE046.py
+++ Exercicios/Mundo 2/EXE046.py
@@ -2,8 +2,8 @@ import time
 
 print('{:=^50}'.format(' EXE 046 '))
 
-for c in range(10, 1, -1):
+for c in range(10, -1, -1):
     print(c)
     time.sleep(1)
-print('Fim')
+print('bum! bum! poow! 🎆')
 print('=' * 50)
