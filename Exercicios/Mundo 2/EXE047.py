ead7821a0b04df732177021c3dd535b0386bffc2
Leonardo Santos <leo.o.d3v@gmail.com>
2026-03-05 18:56:33 -0300

    refactor: simplifica contagem de números pares no EXE047

--- Exercicios/Mundo 2/EXE047.py
+++ Exercicios/Mundo 2/EXE047.py
@@ -1,8 +1,7 @@
 print('{:=^50}'.format(' EXE 047 '), end='\n\n')
-for c in range(1,51):
-    if c % 2 == 0:
-        print((c),' ' ,end='')
-        if c % 10 == 0:
-            print()
+for c in range(2, 51, 2):
+    print((c),' ' ,end='')
+    if c % 10 == 0:
+        print()
 print(('\nFIM!'), end='\n')
 print('=' * 50)
